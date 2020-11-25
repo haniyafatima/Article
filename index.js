7eab427a2c7b9791c8ebf2fbb6655e761e7b034e
haniyafatima <66930662+haniyafatima@users.noreply.github.com>
2020-11-25 15:13:34 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,64 @@
+var shareDisplay=document.querySelector(".share-display");
+var shareBtn= document.querySelector("#share-btn");
+
+
+
+shareBtn.addEventListener("click" ,function(){
+  shareDisplay.classList.toggle("share-toggle");
+} );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
+// function buttonClick(){
+// shareBtn.addEventListener("click" , function(){
+// if(shareDisplay.style.display==="block"){
+//   shareDisplay.style.display="none";
+// }else{
+//   shareDisplay.style.display="block";
+// }
+//
+// });
+// }
